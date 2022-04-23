4195fba792ee084ebf0131e5db466b89b92267b2
sigeharu_mtmo <62659450+sigeharu@users.noreply.github.com>
2022-04-23 09:31:45 +0900

    変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { supabase } from '../utils/supabase'
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
   switch (metric.name) {
@@ -14,7 +19,7 @@ export function reportWebVitals(metric: NextWebVitalsMetric) {
       break
     case 'Next.js-hydration':
       console.log(
-        `Hydration: ${Math.round(metric.value * 10) / 10} -> ${
+        `Hydration: ${Math.round(metric.startTime * 10) / 10} -> ${
           Math.round((metric.startTime + metric.value) * 10) / 10
         }`
       )
@@ -23,8 +28,41 @@ export function reportWebVitals(metric: NextWebVitalsMetric) {
       break
   }
 }
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { push, pathname } = useRouter()
+  const validateSession = async () => {
+    const user = supabase.auth.user()
+    if (user && pathname === '/') {
+      push('/dashboard')
+    } else if (!user && pathname !== '/') {
+      await push('/')
+    }
+  }
+  supabase.auth.onAuthStateChange((event, _) => {
+    if (event === 'SIGNED_IN' && pathname === '/') {
+      push('/dashboard')
+    }
+    if (event === 'SIGNED_OUT') {
+      push('/')
+    }
+  })
+  useEffect(() => {
+    validateSession()
+  }, [])
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
